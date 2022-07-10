5f911b42e243ccda4329eaaad80ad330f3eb5c2a
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-07-10 11:44:49 +0530

    client server app

--- Client_Server_App_TCP/src/TCPClient.java
+++ Client_Server_App_TCP/src/TCPClient.java
@@ -1,5 +1,29 @@
-public class TCPClient {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
+public class TCPClient {
+    public static void main(String[] args) throws Exception {
+        String clientInput, serverInput;
+
+//        To get client input
+//        System.in get as input stream type object, and it stores in buffer reader type object
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("localhost", 9806);
+        System.out.println("Enter a string:");
+//      To send to server
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+//      To get data from server
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//      After user input done get that in to string object
+        clientInput = inFromClient.readLine();
+//        To send it to server
+        outToServer.writeBytes(clientInput + '\n');
+//        Get come from server
+        serverInput = inFromServer.readLine();
+        System.out.println("From Server:" + serverInput);
+//        Finally close the socket
     }
 }
--- Client_Server_App_TCP/src/TCPServer.java
+++ Client_Server_App_TCP/src/TCPServer.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPServer {
     public static void main(String[] args) throws Exception{
@@ -10,6 +12,20 @@ public class TCPServer {
         ServerSocket welcomeSocket = new ServerSocket(9806);
 
         while (true){
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("Connection is established");
+            System.out.println("Getting Client Data");
+//            For read client input
+//            Read the input from socket input stream, and it stores in Buffer reader type object
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//            To send data to client from socket output stream
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientInput = inFromClient.readLine();
+            serverOutput = clientInput.toUpperCase()+ '\n';
+//            This output send byte by byte
+            outToClient.writeBytes(serverOutput);
+            System.out.println("Client Input:" + clientInput);
 
         }
     }
