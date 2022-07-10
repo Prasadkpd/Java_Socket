a9e6c7ad7b1315982fc8af3768dbac3dd7f16d36
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-07-10 10:42:13 +0530

    some changes

---
+++ Client_Server_App_TCP/src/TCPServer.java
@@ -0,0 +1,16 @@
+import java.net.ServerSocket;
+
+public class TCPServer {
+    public static void main(String[] args) throws Exception{
+        String clientInput;
+        String serverOutput;
+
+        System.out.println("Waiting for clients");
+
+        ServerSocket welcomeSocket = new ServerSocket(9806);
+
+        while (true){
+
+        }
+    }
+}
